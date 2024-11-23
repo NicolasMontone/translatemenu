f96f36b6385896d4459c0899a3c851fc50a5b02f
NicolasMontone <montonenicolas01@gmail.com>
2024-11-23 19:27:45 -0300

    confetti

--- app/app/app.tsx
+++ app/app/app.tsx
@@ -4,13 +4,25 @@ import MenuRecommender from '@/components/menu-recommender'
 import Preferences from '@/components/preferences'
 import { useAppContext } from '@/state'
 import { useState } from 'react'
+import { useWindowSize } from 'usehooks-ts'
+import Confetti from 'react-confetti'
 
-export default function App() {
+export default function App({ newCustomer }: { newCustomer: boolean }) {
   const [changingPreferences, setChangingPreferences] = useState(false)
   const { preferences } = useAppContext()
 
+  const { width, height } = useWindowSize()
+
   return (
     <div className="w-full min-h-screen dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      {newCustomer && (
+        <Confetti
+          recycle={false}
+          numberOfPieces={300}
+          width={width}
+          height={height}
+        />
+      )}
       {preferences && !changingPreferences ? (
         <MenuRecommender
           onChangePreferences={() => setChangingPreferences(true)}
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,12 @@
 import App from './app'
 
-export default function AppPage() {
-  return <App />
+export default function AppPage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+  // newCustomer=true
+  return <App newCustomer={searchParams?.newCustomer === 'true'} />
 }
